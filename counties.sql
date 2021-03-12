8636d882fdad766c8bf5ecc15a84fd1bf38e6ee8
Geoffrey Karani <geoffrey.kamundi@gmail.com>
2021-03-12 14:22:19 +0300

    Descriptions

--- counties.sql
+++ counties.sql
@@ -1,56 +1,112 @@
-CREATE TABLE IF NOT EXISTS `counties` (
-  `county_code` INT(11) NOT NULL AUTO_INCREMENT,
-  `county_name` VARCHAR(30) NOT NULL,
-  PRIMARY KEY (`county_code`)
-) ENGINE=INNODB AUTO_INCREMENT=48 DEFAULT CHARSET=latin1;
+-- phpMyAdmin SQL Dump
+-- version 4.9.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 12, 2021 at 06:21 AM
+-- Server version: 10.3.27-MariaDB-log-cll-lve
+-- PHP Version: 7.3.6
 
-/*Data for the table `county` */
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-INSERT  INTO `counties`(`county_code`,`county_name`) 
-VALUES (1,'Mombasa'),
-(2,'Kwale'),
-(3,'Kilifi'),
-(4,'Tana River'),
-(5,'Lamu'),
-(6,'Taita-Taveta'),
-(7,'Garissa'),
-(8,'Wajir'),
-(9,'Mandera'),
-(10,'Marsabit'),
-(11,'Isiolo'),
-(12,'Meru'),
-(13,'Tharaka-Nithi'),
-(14,'Embu'),
-(15,'Kitui'),
-(16,'Machakos'),
-(17,'Makueni'),
-(18,'Nyandarua'),
-(19,'Nyeri'),
-(20,'Kirinyaga'),
-(21,'Murang\'a'),
-(22,'Kiambu'),
-(23,'Turkana'),
-(24,'West Pokot'),
-(25,'Samburu'),
-(26,'Trans-Nzoia'),
-(27,'Uasin Gishu'),
-(28,'Elgeyo-Marakwet'),
-(29,'Nandi'),
-(30,'Baringo'),
-(31,'Laikipia'),
-(32,'Nakuru'),
-(33,'Narok'),
-(34,'Kajiado'),
-(35,'Kericho'),
-(36,'Bomet'),
-(37,'Kakamega'),
-(38,'	Vihiga'),
-(39,'Bungoma'),
-(40,'Busia'),
-(41,'Siaya'),
-(42,'Kisumu'),
-(43,'Homa Bay'),
-(44,'Migori'),
-(45,'Kisii'),
-(46,'Nyamira'),
-(47,'Nairobi');
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `larggucu_elastic_network`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `counties`
+--
+
+CREATE TABLE `counties` (
+  `county_code` int(11) NOT NULL,
+  `county_name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `counties`
+--
+
+INSERT INTO `counties` (`county_code`, `county_name`) VALUES
+(1, 'Mombasa'),
+(2, 'Kwale'),
+(3, 'Kilifi'),
+(4, 'Tana River'),
+(5, 'Lamu'),
+(6, 'Taita-Taveta'),
+(7, 'Garissa'),
+(8, 'Wajir'),
+(9, 'Mandera'),
+(10, 'Marsabit'),
+(11, 'Isiolo'),
+(12, 'Meru'),
+(13, 'Tharaka-Nithi'),
+(14, 'Embu'),
+(15, 'Kitui'),
+(16, 'Machakos'),
+(17, 'Makueni'),
+(18, 'Nyandarua'),
+(19, 'Nyeri'),
+(20, 'Kirinyaga'),
+(21, 'Murang\'a'),
+(22, 'Kiambu'),
+(23, 'Turkana'),
+(24, 'West Pokot'),
+(25, 'Samburu'),
+(26, 'Trans-Nzoia'),
+(27, 'Uasin Gishu'),
+(28, 'Elgeyo-Marakwet'),
+(29, 'Nandi'),
+(30, 'Baringo'),
+(31, 'Laikipia'),
+(32, 'Nakuru'),
+(33, 'Narok'),
+(34, 'Kajiado'),
+(35, 'Kericho'),
+(36, 'Bomet'),
+(37, 'Kakamega'),
+(38, '	Vihiga'),
+(39, 'Bungoma'),
+(40, 'Busia'),
+(41, 'Siaya'),
+(42, 'Kisumu'),
+(43, 'Homa Bay'),
+(44, 'Migori'),
+(45, 'Kisii'),
+(46, 'Nyamira'),
+(47, 'Nairobi');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `counties`
+--
+ALTER TABLE `counties`
+  ADD PRIMARY KEY (`county_code`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `counties`
+--
+ALTER TABLE `counties`
+  MODIFY `county_code` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
